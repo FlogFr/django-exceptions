4ae53693ef208ddf5639041511ca3be494084089
aRkadeFR <fgrignon@online.net>
2014-10-05 21:15:19 +0200

    new version 0.4

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import (
+    setup,
+)
 
 setup(
     name='django-exceptions',
-    version='0.3',
+    version='0.4',
     author='aRkadeFR',
     author_email='contact@arkade.info',
-    url='http://arkadefr.github.io/',
+    url='https://github.com/aRkadeFR/django-exceptions',
+    download_url='https://github.com/aRkadeFR/django-exceptions/tarball/v0.4',
     description='Django app to handle exceptions in a comfortable way',
-    packages=['djexceptions'],
+    packages=['django_exceptions'],
     package_dir={'': 'project'},
     classifiers=[
         'Framework :: Django',
